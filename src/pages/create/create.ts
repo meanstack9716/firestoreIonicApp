66b2452ce00286b802bdf2740c374c02260d113f
meanstack9716 <meanstack9716@gmail.com>
2018-04-23 22:19:23 +0530

    Added create song page, linked it to firestore.

--- src/pages/create/create.ts
+++ src/pages/create/create.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the CreatePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import {
+  IonicPage,
+  NavController,
+  Loading,
+  LoadingController,
+  AlertController,
+  Alert,
+} from 'ionic-angular';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FirestoreProvider } from '../../providers/firestore/firestore';
 
 @IonicPage()
 @Component({
@@ -14,12 +16,50 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'create.html',
 })
 export class CreatePage {
+  public createSongForm: FormGroup; // This is the form we're creating.
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public loadingCtrl: LoadingController,
+    public alertCtrl: AlertController,
+    public firestoreProvider: FirestoreProvider,
+    formBuilder: FormBuilder
+  ) {
+    this.createSongForm = formBuilder.group({
+      albumName: ['', Validators.required],
+      artistName: ['', Validators.required],
+      songDescription: ['', Validators.required],
+      songName: ['', Validators.required],
+    });
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CreatePage');
+  createSong(): void {
+    const loading: Loading = this.loadingCtrl.create();
+    loading.present();
+
+    const albumName = this.createSongForm.value.albumName;
+    const artistName = this.createSongForm.value.artistName;
+    const songDescription = this.createSongForm.value.songDescription;
+    const songName = this.createSongForm.value.songName;
+
+    this.firestoreProvider
+      .createSong(albumName, artistName, songDescription, songName)
+      .then(
+        () => {
+          loading.dismiss().then(() => {
+            this.navCtrl.pop();
+          });
+        },
+        error => {
+          loading.dismiss().then(() => {
+            const alert: Alert = this.alertCtrl.create({
+              message: error.message,
+              buttons: [{ text: 'Ok', role: 'cancel' }],
+            });
+            alert.present();
+          });
+        }
+      );
   }
 
 }
--- src/providers/firestore/firestore.ts
+++ src/providers/firestore/firestore.ts
@@ -1,17 +1,25 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { Song } from '../../models/song.interface';
 
-/*
-  Generated class for the FirestoreProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class FirestoreProvider {
+  constructor(public firestore: AngularFirestore) { }
 
-  constructor(public http: HttpClient) {
-    console.log('Hello FirestoreProvider Provider');
+  createSong(
+    albumName: string,
+    artistName: string,
+    songDescription: string,
+    songName: string
+  ): Promise<void> {
+    const id = this.firestore.createId();
+
+    return this.firestore.doc(`songList/${id}`).set({
+      id,
+      albumName,
+      artistName,
+      songDescription,
+      songName,
+    });
   }
-
 }
